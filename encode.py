5aadac2c5c20d9bbb9a3ba5552457066341bc18f
AllanDiaz <allandiaz31@gmail.com>
2017-03-06 12:12:04 +0100

    Ajout de la matrice de codage + creation des mots

--- encode.py
+++ encode.py
@@ -1,12 +1,23 @@
-import os
+import string
+import re
 
 #lecture du fichier source.txt
-fichier_source = open("source.txt", "r")
-contenu_source = fichier_source.read()
+f_source = open("source.txt", "r")
+contenu_source = f_source.read()
+
+#Creation de mots de taille 3
+mots = re.findall('...', contenu_source)
+
+#Creation d'une matrice de codage
+f_matrice_codage = open("matrice_codage.txt", "r")
+contenu_matrice_codage = f_matrice_codage.read()
+
 
 #TEST
 print(contenu_source)
+print(mots)
+print(contenu_matrice_codage)
 
-toto
-
-fichier_source.close()
+#Fermeture des fichiers ouverts
+f_matrice_codage.close()
+f_source.close()
