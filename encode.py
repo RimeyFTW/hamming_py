3e5ec79c764173bf98b58209d494f448f205d8c1
AllanDiaz <allandiaz31@gmail.com>
2017-03-06 12:34:38 +0100

    Ajout en encode

--- encode.py
+++ encode.py
@@ -1,22 +1,32 @@
 import string
 import re
 
+#Methode addition binaire
+def add(a, b):
+    if a == b:
+        return 0
+    else :
+        return a+b
+
 #lecture du fichier source.txt
 f_source = open("source.txt", "r")
 contenu_source = f_source.read()
 
-#Creation de mots de taille 3
-mots = re.findall('...', contenu_source)
-
-#Creation d'une matrice de codage
+#Lecture d'une matrice de codage
 f_matrice_codage = open("matrice_codage.txt", "r")
 contenu_matrice_codage = f_matrice_codage.read()
 
+#Creation de mots de taille 3
+mots = re.findall('...', contenu_source)
+
+for element_matrice in contenu_matrice_codage:
+    print (element_matrice)
 
 #TEST
 print(contenu_source)
 print(mots)
 print(contenu_matrice_codage)
+
 
 #Fermeture des fichiers ouverts
 f_matrice_codage.close()
